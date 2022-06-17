1d1abdaa65ec49b3edb975548c16eab0aba2bb3d
briotisk <86500240+briotisk@users.noreply.github.com>
2022-06-17 09:37:14 -0300

    Update ctrlF.c

--- ctrlF.c
+++ ctrlF.c
@@ -2,20 +2,26 @@
 #include<stdlib.h>
 #include<string.h>
 
-void LerPrimeiraLinha(char **primeira_linha, FILE *arquivo) {
+void LerLinha(char **primeira_linha, FILE *arquivo) {
 
-    size_t linha_len = 64;
+    //Variaveis auxiliares para controlar o tamanho e acessar uma posicao da string que armazenara o texto do arquivo
+    size_t linha_len = 32;
     size_t char_index = 0;
 
     *primeira_linha = malloc(sizeof(char)*linha_len);
 
+    //O laço é executado até que se chegue ao fim de uma linha(\n) ou ao fim de um arquivo(!feof)
     while (!feof(arquivo) && (*primeira_linha)[char_index - 1] != '\n') {
-
+        
+        //Se o ponteiro estiver na última posicao da string e feita a realocacao
         if (linha_len == char_index) {
-            linha_len *= 16;
+            
+            linha_len += 16;
             *primeira_linha = realloc(*primeira_linha, sizeof(char)*linha_len);
+        
         }
 
+        //A copia e feita caracter a caracter
         (*primeira_linha)[char_index] = fgetc(arquivo);
         char_index++;
     }
@@ -25,47 +31,49 @@ void LerPrimeiraLinha(char **primeira_linha, FILE *arquivo) {
 
 
 void Ctrl_F(char* nome_texto, char* nome_trechos, char* nome_saida){
-    /*
-        Entrada: arquivo trechos, arquivo texto, arquivo saida
-    */
 
    FILE* arq_texto = fopen(nome_texto, "r");
    FILE* arq_trechos = fopen(nome_trechos, "r");
-   FILE* arq_saida = fopen(nome_saida, "a");
+   FILE* arq_saida = fopen(nome_saida, "w");
 
    char *trecho, *texto, *texto_aux;
    texto = malloc(sizeof(char)*1);
 
+   //O laço é executado até que se chegue ao fim do arquivo copiando linha a linha e concatenando em uma string
    while (!feof(arq_texto)){
        
-       LerPrimeiraLinha(&texto_aux, arq_texto);
+       LerLinha(&texto_aux, arq_texto);
        texto = realloc(texto, (sizeof(char)*(strlen(texto)+strlen(texto_aux))+1));
        strcat(texto, " ");
        strcat(texto, texto_aux);
+    
     }
 
-   while(!feof(arq_trechos)){
+    while(!feof(arq_trechos)){
        
-       LerPrimeiraLinha(&trecho, arq_trechos);  
-       
-       int i = 0;
+        //Realiza a leitura da string a ser localizada
+        LerLinha(&trecho, arq_trechos);  
+    
+        int i = 0;
 
-       while (texto[i] != '\0' && trecho[0] != NULL){
+        //A busca e realizada ate que se chegue ao fim do texto
+        //A condicao "trecho[0] != NULL" deve-se a uma leitura residual do "fgetc" da funcao "LerLinha" ao final do arquivo 
+        while (texto[i] != '\0' && trecho[0] != NULL){
+            
+            int j = 0;
+            
+            while ((trecho[j] != '\0') && (texto[i+j] == trecho[j]))
+                j++;
+        
+            if(trecho[j] == '\0'){
 
-           int j = 0;
-
-           while ((trecho[j] != '\0') && (texto[i+j] == trecho[j]))
-               j++;
-           
-           if(trecho[j] == '\0'){
-
-               fprintf(arq_saida,"%d, %d\n", (i-1), (j+i-2));
-               //break;
+                fprintf(arq_saida,"%d, %d\n", (i-1), (j+i-2));
+                break;
 
             }
-
-           i++;        
-           
+        
+            i++;        
+        
         }
        
     }
